1d59f6bf345e075b768b9e9b58628f28346fed5b
Yeoh Khang Sian <alanyeoh0412@gmail.com>
2026-08-22 09:54:13 +0800

    Rename chatbot to Rocky

---
+++ src/main/java/Rocky.java
@@ -0,0 +1,11 @@
+public class Rocky {
+    static void main(String[] args) {
+        String banner = " ____             _          \n"
+                + "|  _ \\ ___   ___| | ___   _ \n"
+                + "| |_) / _ \\ / __| |/ / | | |\n"
+                + "|  _ < (_) | (__|   <| |_| |\n"
+                + "|_| \\_\\___/ \\___|_|\\_\\__, |\n"
+                + "                         |___/\n";
+        System.out.println(banner);
+    }
+}
