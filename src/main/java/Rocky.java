27861e1936d1e8d776fae4d5c41a6888c270a016
Yeoh Khang Sian <alanyeoh0412@gmail.com>
2026-08-22 10:31:38 +0800

    Add List feature to Rocky

--- src/main/java/Rocky.java
+++ src/main/java/Rocky.java
@@ -1,5 +1,7 @@
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 public class Rocky {
     public static void main(String[] args) {
         String banner = " ____             _          \n"
@@ -16,13 +18,28 @@ public class Rocky {
         System.out.println("Amaze, what a special human being! What rocky do for you?");
         System.out.println(divider);
         Scanner scanner = new Scanner(System.in);
+        List<String> statements = new ArrayList<>();
         while (scanner.hasNextLine()) {
-            String userInput = scanner.nextLine();
+            String userInput = scanner.nextLine().trim();
             if (Objects.equals(userInput, "bye")) {
                 break;
             }
+            if (Objects.equals(userInput, "list")) {
+                System.out.println(divider);
+                if (statements.isEmpty()){
+                    System.out.println("Rocky don't see anything!");
+                }
+                else {
+                    for (int i = 0; i < statements.size(); i++) {
+                        System.out.println((i + 1) + ". " + statements.get(i));
+                    }
+                    System.out.println(divider);
+                    continue;
+                }
+            }
             System.out.println(divider);
-            System.out.println(userInput + ", but what it mean?");
+            System.out.println("added: " + userInput + ", but what it mean?");
+            statements.add(userInput);
             System.out.println(divider);
         }
         System.out.println("Bye. We meet again soon!");
