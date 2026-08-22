f0e06d91aef283f18d3f00eb5529389905a45e0f
Yeoh Khang Sian <alanyeoh0412@gmail.com>
2026-08-22 09:57:44 +0800

    Add Greet and Exit

--- src/main/java/Rocky.java
+++ src/main/java/Rocky.java
@@ -1,11 +1,27 @@
+/**
+ * A chatbot that displays a greeting and then exits.
+ */
 public class Rocky {
-    static void main(String[] args) {
+    /**
+     * Starts the chatbot and prints its greeting and farewell messages.
+     *
+     * @param args command-line arguments, which this program does not use
+     */
+    public static void main(String[] args) {
         String banner = " ____             _          \n"
                 + "|  _ \\ ___   ___| | ___   _ \n"
                 + "| |_) / _ \\ / __| |/ / | | |\n"
                 + "|  _ < (_) | (__|   <| |_| |\n"
                 + "|_| \\_\\___/ \\___|_|\\_\\__, |\n"
                 + "                         |___/\n";
+        String divider = "____________________________________________________________";
+
+        System.out.println(divider);
         System.out.println(banner);
+        System.out.println("Hello! I Rocky.");
+        System.out.println("Amaze, what a special human being! What rocky do for you?");
+        System.out.println(divider);
+        System.out.println("Bye. We meet again soon!");
+        System.out.println(divider);
     }
 }
